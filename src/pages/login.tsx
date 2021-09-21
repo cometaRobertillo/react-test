e19fc97538038b6a5a4f06234871bba7d5a929df
jvalen92 <jvalen92@eafit.edu.co>
2021-09-21 16:11:31 -0500

    login template with scss

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,9 +1,53 @@
 import React from 'react'
+import './login.scss'
 
 export const Login = () => {
     return (
-        <div>
-            <h1> Login Screen </h1>
+        <div className="login__main">
+            <div className="login__box-container">
+                <div className="box-container__info">
+
+                    <div className="info__item">Info</div>
+                    <div className="info__item">Info</div>
+                    <div className="info__item">Info</div>
+
+                </div>
+
+                <div className="box-container__form">
+
+                    <h2 className="form__title">Log in</h2>
+                    <form>
+                        <label>
+                            Email
+                        </label>
+                        <input
+                            className="form--input"
+                            placeholder="email"
+                            name="email"
+                            autoComplete="off"
+                        />
+
+                        <label>
+                            Password
+                        </label>
+
+                        <input
+                            className="form--input"
+                            placeholder="password"
+                            name="password"
+                            autoComplete="off"
+                        />
+
+                        <button
+                            type="submit"
+                            className="btn"
+                        >
+                            Login
+                        </button>
+                    </form>
+                </div>
+            </div>
+
         </div>
     )
 }
